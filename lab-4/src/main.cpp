f310c770d8decc610d3d3c3d5179129249ff6187
Plush-Jill <bockarevvlad23@gmail.com>
2025-05-31 01:20:55 +0700

    asd

--- lab-4/src/main.cpp
+++ lab-4/src/main.cpp
@@ -3,7 +3,7 @@
 int main() {
     MemoryBenchmark<char>::test_for_type();
     // MemoryBenchmark<__m512>::test_for_type();
-    // MemoryBenchmark<__m256>::test_for_type();
+    MemoryBenchmark<__m256>::test_for_type();
     MemoryBenchmark<double>::test_for_type();
     MemoryBenchmark<float>::test_for_type();
     MemoryBenchmark<int>::test_for_type();
