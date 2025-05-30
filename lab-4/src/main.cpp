d6a09ed26775088658303c283af0c90ef1e065c8
Plush-Jill <bockarevvlad23@gmail.com>
2025-05-31 02:19:11 +0700

    asd

--- lab-4/src/main.cpp
+++ lab-4/src/main.cpp
@@ -1,15 +1,15 @@
 #include "template-memory-benchmark.hpp"
 
 int main() {
-#ifndef SERVER
-    MemoryBenchmark<__m512>::test_for_type();
-    MemoryBenchmark<__m256>::test_for_type();
-#endif
-
-    MemoryBenchmark<__m128>::test_for_type();
-    MemoryBenchmark<double>::test_for_type();
-    MemoryBenchmark<float>::test_for_type();
-    MemoryBenchmark<int>::test_for_type();
     MemoryBenchmark<char>::test_for_type();
+// #ifndef SERVER
+    // MemoryBenchmark<__m512>::test_for_type();
+    // MemoryBenchmark<__m256>::test_for_type();
+// #endif
+
+    // MemoryBenchmark<__m128>::test_for_type();
+    // MemoryBenchmark<double>::test_for_type();
+    // MemoryBenchmark<float>::test_for_type();
+    // MemoryBenchmark<int>::test_for_type();
     return 0;
 }
